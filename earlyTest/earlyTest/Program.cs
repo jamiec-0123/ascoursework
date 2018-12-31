e5acd19de196fd91eaa1abd77fdbaeae5a3dd9fc
JamieC <theindiedreamdev@gmail.com>
2018-12-31 10:16:36 +0000

    Backing up all progress, work on main game

--- earlyTest/earlyTest/Program.cs
+++ earlyTest/earlyTest/Program.cs
@@ -16,7 +16,7 @@ namespace earlyTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new loginScreen());
         }
     }
 }
