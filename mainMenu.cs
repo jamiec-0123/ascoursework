b58782134278d0a1273bfcb802d8bc6e0a937c97
Jamie C <46240185+jamiec-0123@users.noreply.github.com>
2019-01-19 18:26:35 +0000

    Add files via upload
    Terminal is not working...
    Working on the main game

---
+++ mainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace earlyTest
+{
+    public partial class mainMenu : Form
+    {
+        public mainMenu(bool teacher)
+        {
+            if (teacher == true)
+            {InitializeComponent();
+                quitBTN.Location = new Point(218, 410);
+                teacherBTN.Visible= true;
+            }
+            else
+            {
+            InitializeComponent();
+            }
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void quitBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void startGameBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void highscoreBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teacherBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
